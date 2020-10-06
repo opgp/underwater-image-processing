90b61a9c7faf0a74b1acf13095ef8172069cfd87
Aadhar <tyagi.aadhar@gmail.com>
2020-10-06 08:26:39 +0530

    convert cpp to header files

---
+++ src/imageio.h
@@ -0,0 +1,31 @@
+#ifndef imageio
+#define imageio
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include <bits/stdc++.h>
+#include <debug.h>
+
+using namespace std;
+using namespace cv;
+
+Mat getImage (char* imgNum, char* type, bool color = 1)
+{
+  string imagePath = "./dataset/" + string(type) + "/" + string(imgNum) + ".png";
+  Mat image = imread (imagePath, IMREAD_COLOR);
+
+  if(image.empty()) {
+    cout << "Image not found or unable to open at " << imagePath << endl;
+    exit(-1);
+  }
+  // resize (image, image, Size(640, 480), 0, 0, INTER_CUBIC);
+
+  return image;
+}
+
+void showImage (string name, Mat image)
+{
+  namedWindow( name, WINDOW_AUTOSIZE );
+  imshow(name, image );
+}
+#endif
