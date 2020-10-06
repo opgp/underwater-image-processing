180cc6814578d27edfd0656d5ee68d3ad9444e2b
Aadhar <tyagi.aadhar@gmail.com>
2020-10-06 10:08:40 +0530

    add benchmarking

---
+++ src/imageEvaluate.h
@@ -0,0 +1,63 @@
+#ifndef imageEvaluate
+#define imageEvaluate
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string execute(string str)
+{
+  FILE *fpipe;
+  // Convert string to char*
+  char *command = const_cast <char*> (str.c_str());
+  char c = 0;
+
+  // Opening a pipe with given command
+  if (0 == (fpipe = (FILE*)popen(command, "r")))
+  {
+      perror("popen() failed.");
+      exit(EXIT_FAILURE);
+  }
+
+  // Reading std output and storing in a string
+  string output;
+  while (fread(&c, sizeof c, 1, fpipe))
+  {
+      output += c;
+  }
+
+  pclose(fpipe);
+  return output;
+}
+
+void rEval(char* imgName, char* type)
+{
+  // Create paths for orignial and reference images
+  string original = "./dataset/" + string(type) + "/" + string(imgName) + ".png";
+  string reference = "./dataset/reference/" + string(imgName) + ".png";
+
+  // Metrics avalailable in sewar library
+  // refer https://github.com/andrewekhalel/sewar
+  vector <string> metrics = {
+    "mse",
+    // "rmse",
+    "psnr",
+    // "rmse",
+    // "sw",
+    // "uqi",
+    // "ssim",
+    // "ergas",
+    // "scc",
+    // "rase",
+    // "sam",
+    // "msssim",
+    // "vifp",
+    // "psnrb"
+  };
+
+
+  for (auto metric: metrics)
+    cout << execute ("sewar " + metric + " " + reference + " " + original) << endl;
+
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,24 @@
 #include <bits/stdc++.h>
 #include "imageio.h"
 #include "imageAlgo.h"
+#include "imageEvaluate.h"
 
 using namespace std;
 using namespace cv;
 
 int main( int argc, char** argv ) {
 
-  Mat image = getImage(argv[2], argv[1]);
+  
+  // Mat image = getImage(argv[2], argv[1]);
 
-  showImage("normal", image);
+  // showImage("normal", image);
+  //
+  // enhance (image);
+  //
+  // showImage("enhanced", image);
 
-  enhance (image);
+  rEval(argv[2], argv[1]);
 
-  showImage("enhanced", image);
-
-  waitKey(0);
+  // waitKey(0);
   return 0;
 }
