c02db5e8bf80b036de4495088106d1e6a4276221
Aadhar <tyagi.aadhar@gmail.com>
2020-11-29 17:36:30 +0530

    add white balancing

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,34 @@
 #include "imageAlgo.h"
 #include "imageEvaluate.h"
 
+using namespace std::chrono;
 using namespace std;
 using namespace cv;
 
 int main( int argc, char** argv ) {
 
-  
-  // Mat image = getImage(argv[2], argv[1]);
+  Mat image = getImage(argv[2], argv[1]);
 
-  // showImage("normal", image);
-  //
-  // enhance (image);
-  //
-  // showImage("enhanced", image);
+  auto start = high_resolution_clock::now();
 
-  rEval(argv[2], argv[1]);
+  showImage("normal", image);
+  imwrite ("original.png", image);
 
-  // waitKey(0);
+  enhance (image);
+  showImage("not White balanced", image);
+
+  whiteBalancing(image);
+
+  imwrite ("whiteBalanced.png", image);
+
+  auto stop = high_resolution_clock::now();
+
+  auto duration = duration_cast<microseconds>(stop - start);
+
+  cout << "Time taken by function: "
+       << duration.count() << " microseconds" << endl;
+  showImage("enhanced", image);
+
+  waitKey(0);
   return 0;
 }
