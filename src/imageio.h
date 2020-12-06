3058c8c5b15dafd533b7ce828fb9a8cf07550cb4
Aadhar <tyagi.aadhar@gmail.com>
2020-12-06 17:16:57 +0530

    update image paths

--- src/imageio.h
+++ src/imageio.h
@@ -5,29 +5,37 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
 #include <bits/stdc++.h>
-#include <debug.h>
 
 using namespace std;
 using namespace cv;
 
-Mat getImage (char* imgNum, char* type, bool color = 1)
+string extractImageName (string path)
 {
-  string imagePath = "./dataset/" + string(type) + "/" + string(imgNum) + ".png";
+  int n = path.size();
+  string name;
+  for (int i = n-1; i >= 0 && path[i] != '/'; i--)
+    name.push_back(path[i]);
+  reverse (name.begin(), name.end());
+  return name;
+}
+
+Mat getImage (string imagePath, bool color = 1)
+{
   Mat image = imread (imagePath);
 
   if(image.empty()) {
     cout << "Image not found or unable to open at " << imagePath << endl;
     exit(-1);
   }
-  
+
   // resize (image, image, Size(640, 480), 0, 0, INTER_CUBIC);
 
   return image;
 }
 
-
-void showImage (string name, Mat image)
+void showImage (string path, Mat image)
 {
+  string name = extractImageName (string(path));
   namedWindow( name, WINDOW_AUTOSIZE );
   imshow(name, image );
 }
