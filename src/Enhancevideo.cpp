facb00e03e14ba6d2de3e699ff970121a695437c
Aadhar <tyagi.aadhar@gmail.com>
2020-12-25 21:17:14 +0530

    add video support

--- src/Enhancevideo.cpp
+++ src/Enhancevideo.cpp
@@ -1,29 +1,53 @@
-//#include <stdio.h>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-//#include <iostream>
+#include "opencv2/opencv.hpp"
+#include "imageAlgo.h"
+#include <iostream>
 
+using namespace std;
 using namespace cv;
-using std::string;
 
-int main(int argc, char** argv)
-{
-    string filename = argv[1];
-    VideoCapture capture(filename);
+int main(int argc, char** argv){
+
+  // Create a VideoCapture object and open the input file
+  // If the input is the web camera, pass 0 instead of the video file name
+  VideoCapture cap(argv[1]);
+
+  // Check if camera opened successfully
+  if(!cap.isOpened()) {
+    cout << "Error opening video stream or file" << endl;
+    return -1;
+  }
+
+  while(1) {
+
     Mat frame;
+    // Capture frame-by-frame
+    cap >> frame;
 
-    if( !capture.isOpened() )
-        throw "Error when reading steam";
+    // If the frame is empty, break immediately
+    if (frame.empty())
+      break;
 
-    namedWindow( "w", 1);
-    for( ; ; )
-    {
-        capture >> frame;
-        if(frame.empty())
-            break;
-        imshow("w", frame);
-        waitKey(20); // waits to display frame
-    }
-    waitKey(0); // key press to close window
-    // releases and window destroy are automatic in C++ interface
+    resize (frame, frame, Size(640, 480), 0, 0, INTER_CUBIC);
+    // Display the resulting frame
+    imshow( "Original", frame );
+
+    // Algorithm
+    enhance (frame);
+    whiteBalancing(frame);
+
+    imshow( "Enhanced", frame );
+
+    // Press  ESC on keyboard to exit
+    char c = (char) waitKey(25);
+    if(c == 27)
+      break;
+  }
+
+  // When everything done, release the video capture object
+  cap.release();
+
+  // Closes all the frames
+  destroyAllWindows();
+
+  return 0;
 }
